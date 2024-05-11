9eacc2abd36fd6ec574a77735f4096d6f5d3b8f3
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-05-11 18:32:26 -0500

    child card

--- src/main/java/com/london/immunizations/controller/ChildController.java
+++ src/main/java/com/london/immunizations/controller/ChildController.java
@@ -4,14 +4,12 @@ import com.london.immunizations.model.Child;
 import com.london.immunizations.model.User;
 import com.london.immunizations.repository.ChildRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 public class ChildController {
 
     @Autowired
