d2fcaddcdd0ed0390374f4c0fbd702478ff9fc6a
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-05-15 15:24:59 -0500

    updated immunization class

--- src/main/java/com/london/immunizations/model/Immunization.java
+++ src/main/java/com/london/immunizations/model/Immunization.java
@@ -3,6 +3,11 @@ package com.london.immunizations.model;
 import java.util.Date;
 
 public class Immunization {
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
 
     private String vaccineGroup;
 
@@ -16,4 +21,81 @@ public class Immunization {
 
     private String vaccineBrand;
 
+    private String doctorName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getVaccineGroup() {
+        return vaccineGroup;
+    }
+
+    public void setVaccineGroup(String vaccineGroup) {
+        this.vaccineGroup = vaccineGroup;
+    }
+
+    public int getDose() {
+        return dose;
+    }
+
+    public void setDose(int dose) {
+        this.dose = dose;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getDateAdministered() {
+        return dateAdministered;
+    }
+
+    public void setDateAdministered(Date dateAdministered) {
+        this.dateAdministered = dateAdministered;
+    }
+
+    public String getVaccineName() {
+        return vaccineName;
+    }
+
+    public void setVaccineName(String vaccineName) {
+        this.vaccineName = vaccineName;
+    }
+
+    public String getVaccineBrand() {
+        return vaccineBrand;
+    }
+
+    public void setVaccineBrand(String vaccineBrand) {
+        this.vaccineBrand = vaccineBrand;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
 }
