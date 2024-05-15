bceee5468e0aed5598615479cd9da80a6e3f65a3
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-05-15 17:06:35 -0500

    minor fix to immunization class

--- src/main/java/com/london/immunizations/controller/ImmunizationController.java
+++ src/main/java/com/london/immunizations/controller/ImmunizationController.java
@@ -1,6 +1,5 @@
 package com.london.immunizations.controller;
 
-import com.london.immunizations.model.Immunization;
 import com.london.immunizations.repository.ImmunizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +13,15 @@ import java.util.List;
         @Autowired
         private ImmunizationRepository immunizationRepository;
 
-        @PostMapping("/add/immunization")
-        Immunization newImmunization (@RequestBody Immunization newImmunization) {
-            return immunizationRepository.save(newImmunization);
-        }
-
-        @GetMapping("/immunizations")
-        List<Immunization> getAllImmunizations() {
-            return immunizationRepository.findAll();
-        }
+//        @PostMapping("/add/immunization")
+//        Immunization newImmunization (@RequestBody Immunization newImmunization) {
+//            return immunizationRepository.save(newImmunization);
+//        }
+//
+//        @GetMapping("/immunizations")
+//        List<Immunization> getAllImmunizations() {
+//            return immunizationRepository.findAll();
+//        }
     }
 
-}
+
--- src/main/java/com/london/immunizations/model/Immunization.java
+++ src/main/java/com/london/immunizations/model/Immunization.java
@@ -1,8 +1,17 @@
 package com.london.immunizations.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Date;
 
+@Entity
 public class Immunization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long childId;
